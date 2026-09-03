125a2a0888b9be767a8cfc82444bc58c71ecbde5
hajonsdyfy <hajonsdyfy@gmail.com>
2026-09-03 22:58:01 +0800

    fix landing redirect

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,6 +2,7 @@ import { NextIntlClientProvider, hasLocale } from "next-intl";
 import { notFound } from "next/navigation";
 import { routing } from "@/i18n/routing";
 import { fontSans } from "@/lib/fonts";
+import { getMessages } from "next-intl/server";
 import "../globals.css";
 
 export default async function LocaleLayout({
@@ -14,10 +15,12 @@ export default async function LocaleLayout({
   const { locale } = await params;
   if (!hasLocale(routing.locales, locale)) notFound();
 
+  const messages = await getMessages();
+
   return (
     <html lang={locale} suppressHydrationWarning className={`${fontSans.variable} h-full antialiased`}>
       <body className="min-h-full flex flex-col">
-        <NextIntlClientProvider>
+        <NextIntlClientProvider messages={messages}>
           {children}
         </NextIntlClientProvider>
       </body>
